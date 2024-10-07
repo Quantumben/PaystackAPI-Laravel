8b34fa861e3f58933a5cd0ed7b83786fc96d8ac6
QuantumBen <omosabenedict@gmail.com>
2024-10-06 21:06:48 -0700

    PaystackPayment Controller

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/paystack/pay', [App\Http\Controllers\PaystackPaymentController::class, 'index'])->name('paystack.index');
+Route::post('/paystack/pay', [App\Http\Controllers\PaystackPaymentController::class, 'initiatePayment'])->name('paystack.pay');
+Route::get('/paystack/callback', [App\Http\Controllers\PaystackPaymentController::class, 'handleCallback'])->name('paystack.callback');
